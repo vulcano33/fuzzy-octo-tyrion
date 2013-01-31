d3673a1920c94841b82b03972800c8883a80d388
Carlos Cabrera <carlos.cabrera@codescrum.com>
2013-01-31 15:51:39 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OctoTyrion::Application.config.secret_token = '0309c7d9aca3e4b4130f8a3860dd5352ac066d258014e40181881ab83887c5a82da7fbb109f9d7c32ebde36a089608cb2f0bf8c9a7315e0ff121c58ac377336d'
